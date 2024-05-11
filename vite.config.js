2740a2bfb1f2b56e2cc75ba7cbe4c5f70f8340e6
sujal kumar <sujalkumar0444@gmail.com>
2024-05-11 09:36:03 +0530

    add github workflow

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true,
-    port:5000
-  },
+  // server:{
+  //   host:true,
+  //   port:5000
+  // },
+  base: "/courser-admin/"
 })
